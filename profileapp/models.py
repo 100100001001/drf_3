5584407999a4ed588b48b4b7eed40396e1c43a2c
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2021-12-23 16:31:07 +0900

    Async task update 2

--- profileapp/models.py
+++ profileapp/models.py
@@ -8,6 +8,9 @@ from io import BytesIO
 
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from image_processing import thumbnail
 from profileapp import tasks
 
@@ -22,12 +25,7 @@ class Profile(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
 
-    def generate_thumbnail(self):
-        if self.image:
-            output = tasks.generate_thumbnail_celery_lag.delay(self.image)
-
-            self.thumb = InMemoryUploadedFile(output, "ImageField", self.image.name,
-                                              'image/jpeg', sys.getsizeof(output), None)
+@receiver(post_save, sender=Profile)
+def async_generate_thumbnail(sender, instance=None, created=False, **kwargs):
+    tasks.generate_thumbnail_celery_lag.delay(instance.pk)
